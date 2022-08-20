30789b768c00ba3976f8375dc5d927c2a3e3d8cb
Sameer Bhilare <bhilare.sameer@gmail.com>
2022-08-20 22:57:23 +0200

    Fetching saved session

--- Workspace/rbay/src/services/queries/sessions.ts
+++ Workspace/rbay/src/services/queries/sessions.ts
@@ -1,5 +1,23 @@
+import { sessionsKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { Session } from '$services/types';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+	const session = await client.hGetAll(sessionsKey(id));
+
+	// redis HGETALL return empty object if given hash does not exist.
+	if (Object.keys.length === 0) {
+		return null;
+	}
+	return deserialize(id, session);
+};
 
 export const saveSession = async (session: Session) => {};
+
+const deserialize = (id: string, session: { [key: string]: string }) => {
+	return {
+		id,
+		userId: session.userId,
+		username: session.username
+	};
+};
