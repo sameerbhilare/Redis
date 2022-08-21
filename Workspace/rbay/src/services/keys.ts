cd0213ef5a768d8b30c74ef740757ac490eb02bf
Sameer Bhilare <bhilare.sameer@gmail.com>
2022-08-21 19:07:13 +0200

    Requiring unique usernames

--- Workspace/rbay/src/services/keys.ts
+++ Workspace/rbay/src/services/keys.ts
@@ -2,3 +2,4 @@ export const pageCacheKey = (id: string) => `page#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
+export const usernameUniqueKey = () => 'usernames:unique';
--- Workspace/rbay/src/services/queries/users.ts
+++ Workspace/rbay/src/services/queries/users.ts
@@ -1,7 +1,7 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey } from '$services/keys';
+import { usernameUniqueKey, usersKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
@@ -12,7 +12,17 @@ export const getUserById = async (id: string) => {
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
+
+	// see if username already exists
+	const exists = await client.sIsMember(usernameUniqueKey(), attrs.username);
+	if (exists) {
+		throw new Error('Username is taken');
+	}
+
 	await client.hSet(usersKey(id), serialize(attrs));
+	// add the created username to unique usernames
+	await client.sAdd(usernameUniqueKey(), attrs.username);
+
 	return id;
 };
 
