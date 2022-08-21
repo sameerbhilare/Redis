bc10779e0c52557344110798480ba232e1fd25ab
Sameer Bhilare <bhilare.sameer@gmail.com>
2022-08-21 19:24:05 +0200

    Updating Like Quantities

--- Workspace/rbay/src/services/queries/likes.ts
+++ Workspace/rbay/src/services/queries/likes.ts
@@ -1,16 +1,24 @@
-import { userLikesKey } from '$services/keys';
+import { itemsKey, userLikesKey } from '$services/keys';
 import { client } from '$services/redis';
 
-export const userLikesItem = async (itemId: string, userId: string) => {};
+export const userLikesItem = async (itemId: string, userId: string) => {
+	await client.sIsMember(userLikesKey(userId), itemId);
+};
 
 export const likedItems = async (userId: string) => {};
 
 export const likeItem = async (itemId: string, userId: string) => {
-	await client.sAdd(userLikesKey(userId), itemId);
+	const inserted = await client.sAdd(userLikesKey(userId), itemId);
+	if (inserted) {
+		return await client.hIncrBy(itemsKey(itemId), 'likes', 1);
+	}
 };
 
 export const unlikeItem = async (itemId: string, userId: string) => {
-	await client.sRem(userLikesKey(userId), itemId);
+	const removed = await client.sRem(userLikesKey(userId), itemId);
+	if (removed) {
+		return await client.hIncrBy(itemsKey(itemId), 'likes', -1);
+	}
 };
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
