f1480e8b9f435332d95c14fe1238839376771958
Sameer Bhilare <bhilare.sameer@gmail.com>
2022-08-24 21:57:47 +0200

    Parsing SORT Output

--- Workspace/rbay/src/services/queries/items/by-views.ts
+++ Workspace/rbay/src/services/queries/items/by-views.ts
@@ -1,10 +1,18 @@
 import { itemsByViewsKey, itemsKey } from '$services/keys';
 import { client } from '$services/redis';
+import { deserialize } from './deserialize';
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-	const results = await client.sort(itemsByViewsKey(), {
+	let results: any = await client.sort(itemsByViewsKey(), {
 		// get id, name and views
-		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
+		GET: [
+			'#',
+			`${itemsKey('*')}->name`,
+			`${itemsKey('*')}->views`,
+			`${itemsKey('*')}->endingAt`,
+			`${itemsKey('*')}->imageUrl`,
+			`${itemsKey('*')}->price`
+		],
 		// no sorting required
 		BY: 'nosort',
 		// direction of the sorting
@@ -13,5 +21,14 @@ export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, c
 		LIMIT: { offset, count }
 	});
 
-	console.log(results);
+	// above command returns plain array of strings in the form of
+	// id1, name1, views1, endingAt1, imageUrl1, price1, id2, name2, views2, endingAt2, imageUrl2, price2, ....
+	// we need to convert this array of unstructured strings to item object
+	const items = [];
+	while (results.length) {
+		const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+		const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+		items.push(item);
+		results = rest;
+	}
 };
