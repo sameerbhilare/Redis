f9e1b23f6ae9d7f2c3d0438267e6972a07b60e66
Sameer Bhilare <bhilare.sameer@gmail.com>
2022-08-24 21:49:44 +0200

    A Few More Arguments

--- Workspace/rbay/src/services/queries/items/by-views.ts
+++ Workspace/rbay/src/services/queries/items/by-views.ts
@@ -3,8 +3,14 @@ import { client } from '$services/redis';
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
 	const results = await client.sort(itemsByViewsKey(), {
+		// get id, name and views
 		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
-		BY: 'score'
+		// no sorting required
+		BY: 'nosort',
+		// direction of the sorting
+		DIRECTION: order,
+		// to limit the number of records to fetch
+		LIMIT: { offset, count }
 	});
 
 	console.log(results);
