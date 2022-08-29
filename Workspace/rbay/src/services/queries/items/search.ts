ffd65fd72e8deef395fc9e338b4d5c974b7ee2b1
Sameer Bhilare <bhilare.sameer@gmail.com>
2022-08-29 17:18:53 +0200

    Parsing the Search Term

--- Workspace/rbay/src/services/queries/items/search.ts
+++ Workspace/rbay/src/services/queries/items/search.ts
@@ -1,1 +1,13 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+import { itemsIndexKey } from '$services/keys';
+
+export const searchItems = async (term: string, size: number = 5) => {
+	// pre-processing user entered input for fuzzy searching
+	const cleaned = term
+		.replaceAll(/[^a-zA-Z0-9 ]/g, '') // replace alpha numberic characters
+		.trim()
+		.split(' ')
+		.map((word) => (word ? `%${word}%` : ''))
+		.join(' ');
+};
